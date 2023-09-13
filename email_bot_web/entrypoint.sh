73bf03e7122dd779f8ffdcca879c341f1795604d
Artem Menshikov <a.menshikov1989@gmail.com>
2023-09-13 12:18:58 +0300

    web_port in env

--- email_bot_web/entrypoint.sh
+++ email_bot_web/entrypoint.sh
@@ -3,4 +3,4 @@
 python3 manage.py migrate
 python3 manage.py collectstatic --noinput
 
-uvicorn email_bot_web.asgi:application --host 0.0.0.0 --port 8000
+uvicorn email_bot_web.asgi:application --host 0.0.0.0 --port $WEB_PORT
