eb8aeaff89dffc464ffc6a8f0475c1e94c538f05
Svetlov-al <s3t1qs@gmail.com>
2023-10-06 17:39:31 +0400

    make new parse func, create better pic from text

--- email_bot_web/infrastucture/email_processor.py
+++ email_bot_web/infrastucture/email_processor.py
@@ -1,43 +1,65 @@
+import io
+import os
 import re
+import uuid
 from typing import Any
 
 from api.services.box_filter_services import BoxFilterService
+from email_service.schema import ImapEmailModel
+from html2image import Html2Image
 from infrastucture.logger_config import logger
-from PIL import Image, ImageDraw, ImageFont
+from PIL import Image
 
 filters = BoxFilterService
-
 
 EMAIL_PATTERN = r'[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}'
 
 
 class EmailToImage:
-    """Создает из текста изображение с содержимым"""
+    """Класс для преобразования из текста/html разметки в изображение с содержимым"""
 
-    def __init__(self, width: int = 800, height: int = 600, font_size: int = 20) -> None:
+    def __init__(self, width: int = 1920, height: int = 1080) -> None:
         self.width = width
         self.height = height
-        self.font_size = font_size
+        self.hti = Html2Image(browser='chromium', keep_temp_files=False)
+        self.hti.browser.flags = [
+            '--no-sandbox',
+            '--headless',
+            '--disable-gpu',
+            '--hide-scrollbars'
+        ]
 
-    def generate_image(self, text: str) -> Image.Image:
+        self.hti.size = (self.width, self.height)
 
-        img = Image.new('RGB', (self.width, self.height), color='white')
-        d = ImageDraw.Draw(img)
+    @classmethod
+    def generate_unique_filename(cls, extension: str = '.png') -> str:
+        """Метод генерации уникального имени файла."""
 
-        font = ImageFont.load_default()
+        return f'{uuid.uuid4()}{extension}'
 
-        # Разбивка текста на строки, чтобы он помещался в изображение
-        y_text = 10
-        line_height = 20
+    def generate_image(self, text: str, save_path: str) -> None:
+        """Метод преобразования текста в изображение с сохранением в контейнере."""
 
-        # Игнорирование символов, которые не поддерживаются шрифтом
-        safe_text = text.encode('latin-1', errors='ignore').decode('latin-1')
-        d.multiline_text((10, y_text), safe_text, font=font, fill='black', spacing=line_height)
+        self.hti.screenshot(html_str=text, save_as=save_path)
 
-        return img
+    def generate_image_to_send(self, text: str) -> io.BytesIO:
+        """Метод преобразования текста в изображение для отправки в байтах."""
+
+        temp_path = self.generate_unique_filename()
+        self.hti.screenshot(html_str=text, save_as=temp_path)
+
+        # Загрузка изображения и сохранение его в байтовый поток
+        image = Image.open(temp_path)
+        byte_stream = io.BytesIO()
+        image.save(byte_stream, format='PNG')
+        byte_stream.seek(0)
+
+        os.remove(temp_path)
+
+        return byte_stream
 
 
-async def process_email(email_object: Any, telegram_id: int, email_username: str) -> None:
+async def process_email(email_object: ImapEmailModel, telegram_id: int, email_username: str) -> None:
     """Обработка письма, сортировка по фильтрам, преобразование в фотографию"""
 
     list_of_filters: list[dict | Any] = await filters.get_filters_for_user_and_email(telegram_id, email_username)
@@ -57,12 +79,13 @@ async def process_email(email_object: Any, telegram_id: int, email_username: str
                 logger.info(f'To: {email_object.to}')
                 logger.info(f'Subject: {email_object.subject}')
                 logger.info(f'Body: {email_object.body}')
-                email_content = f"""
-                        Date: {email_object.date}
-                        From: {email_object.from_}
-                        To: {email_object.to}
-                        Subject: {email_object.subject}
-                        Body: {email_object.body}
-                        """
-                img = email_to_image.generate_image(email_content)
-                img.save('output_image.png')
+
+    email_content = f"""
+    Дата письма: {email_object.date}<br>
+    От кого: {email_object.from_}<br>
+    Кому: {email_object.to}<br>
+    Тема: {email_object.subject}<br>
+    Сообщение: {email_object.body}
+    """
+    email_to_image = EmailToImage()
+    email_to_image.generate_image(email_content, EmailToImage.generate_unique_filename())
