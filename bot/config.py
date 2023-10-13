fcc802e2e2d557560184016f62962c0650179ee6
Svetlov-al <s3t1qs@gmail.com>
2023-10-13 13:38:48 +0400

    move trottling time to env, add filter name, value, email username length validation

--- bot/config.py
+++ bot/config.py
@@ -5,6 +5,8 @@ from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from middleware import BackendServiceMiddleware, ThrottlingMiddleware
 
 ENCRYPTION_KEY = os.getenv('ENCRYPTION_KEY')
+TROTTLING_TIME = int(os.getenv('TROTTLING_TIME', 60))
+
 
 bot = Bot(token=os.getenv('BOT_TOKEN'))
 dp = Dispatcher(bot=bot, storage=MemoryStorage())
